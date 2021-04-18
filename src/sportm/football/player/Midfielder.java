4944027bf849578734670d53858946fcc908b256
Mapc1 <marcocosta579@gmail.com>
2021-04-18 17:49:16 +0100

    Add } at the end of Midfielder.java

--- src/sportm/football/player/Midfielder.java
+++ src/sportm/football/player/Midfielder.java
@@ -15,8 +15,7 @@ public class Midfielder extends FootballPlayer {
                     int kick,
                     int passing,
                     int ballRecovery
-            )
-    {
+            ) {
         super(name, team, speed, resistance, dexterity, implosion, headGame, kick, passing);
         this.changeBallRecovery(ballRecovery);
     }
@@ -30,31 +29,33 @@ public class Midfielder extends FootballPlayer {
         this.setBallRecovery(ballRecovery);
         updateOverallSkill();
     }
+
     private void setBallRecovery(int ballRecovery) {
         this.ballRecovery = ballRecovery;
     }
+
     public int getBallRecovery() {
         return this.ballRecovery;
     }
 
     @Override
     public int calcOverallSkill() {
-        return (int) ((this.getSpeed() * 0.20)      +
+        return (int) ((this.getSpeed() * 0.20) +
                 (this.getResistance() * 0.15) +
-                (this.getDexterity() * 0.05)  +
-                (this.getImplosion() * 0.05)  +
-                (this.getHeadGame() * 0.10)   +
-                (this.getKick() * 0.10)       +
-                (this.getPassing() * 0.15)    +
+                (this.getDexterity() * 0.05) +
+                (this.getImplosion() * 0.05) +
+                (this.getHeadGame() * 0.10) +
+                (this.getKick() * 0.10) +
+                (this.getPassing() * 0.15) +
                 (this.getBallRecovery() * 0.20));
     }
 
     public boolean equals(Midfielder midfielder) {
         boolean ret = false;
 
-        if(this == midfielder)
+        if (this == midfielder)
             ret = true;
-        else if(super.equals(midfielder) && this.getBallRecovery() == midfielder.getBallRecovery())
+        else if (super.equals(midfielder) && this.getBallRecovery() == midfielder.getBallRecovery())
             ret = true;
 
         return ret;
@@ -64,3 +65,4 @@ public class Midfielder extends FootballPlayer {
     public Midfielder clone() {
         return new Midfielder(this);
     }
+}
