dbac6987ffd4da88d890f0f91259f46a0a744a5b
Mapc1 <marcocosta579@gmail.com>
2021-04-18 17:36:44 +0100

    Add Lateral class

---
+++ src/sportm/football/player/Lateral.java
@@ -0,0 +1,68 @@
+package sportm.football.player;
+
+public class Lateral extends FootballPlayer {
+    private int crossing;
+
+    public Lateral
+            (
+                    String name,
+                    String team,
+                    int speed,
+                    int resistance,
+                    int dexterity,
+                    int implosion,
+                    int headGame,
+                    int kick,
+                    int passing,
+                    int crossing
+            )
+    {
+        super(name, team, speed, resistance, dexterity, implosion, headGame, kick, passing);
+        changeCrossing(crossing);
+    }
+
+    public Lateral(Lateral lateral) {
+        super(lateral);
+        changeCrossing(lateral.getCrossing());
+    }
+
+    private void setCrossing(int crossing) {
+        this.crossing = crossing;
+    }
+    public int getCrossing() {
+        return this.crossing;
+    }
+    public void changeCrossing(int crossing) {
+       setCrossing(crossing);
+       updateOverallSkill();
+    }
+
+    @Override
+    public int calcOverallSkill() {
+        return (int) ((this.getSpeed() * 0.15)      +
+                (this.getResistance() * 0.15) +
+                (this.getDexterity() * 0.10)  +
+                (this.getImplosion() * 0.10)  +
+                (this.getHeadGame() * 0.05)   +
+                (this.getKick() * 0.10)       +
+                (this.getPassing() * 0.20)    +
+                (this.getCrossing() * 0.15));
+    }
+
+    public boolean equals(Lateral lateral) {
+
+        boolean ret = false;
+
+        if(this == lateral)
+            ret = true;
+        else if(super.equals(lateral) && this.getCrossing() == lateral.getCrossing())
+            ret = true;
+
+        return ret;
+    }
+
+    @Override
+    public Lateral clone() {
+        return new Lateral(this);
+    }
+}
